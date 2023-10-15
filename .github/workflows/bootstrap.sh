72f0bc3d3b6f5bbec86b1c68c12a7dd953a38554
İlteriş Yağıztegin Eroğlu <ilteris@asenkron.com.tr>
2023-10-15 19:45:01 +0300

    update script
    Signed-off-by: İlteriş Yağıztegin Eroğlu <ilteris@asenkron.com.tr>

--- .github/workflows/bootstrap.sh
+++ .github/workflows/bootstrap.sh
@@ -36,9 +36,9 @@ curl_getopt_pid=$!
 echo "Fetching MacPorts..."
 /usr/bin/curl -fsSLO "https://github.com/macports/macports-ci-files/releases/download/v${MACPORTS_VERSION}/${MACPORTS_FILENAME}" &
 curl_mpbase_pid=$!
-echo "Fetching PortIndex..."
-/usr/bin/curl -fsSLo ports/PortIndex "https://ftp.fau.de/macports/release/ports/PortIndex_darwin_${OS_MAJOR}_${OS_ARCH}/PortIndex" &
-curl_portindex_pid=$!
+#echo "Fetching PortIndex..."
+#/usr/bin/curl -fsSLo ports/PortIndex "https://ftp.fau.de/macports/release/ports/PortIndex_darwin_${OS_MAJOR}_${OS_ARCH}/PortIndex" &
+#curl_portindex_pid=$!
 endgroup
 
 
@@ -115,7 +115,7 @@ git -C ports/ checkout -qf macports/master~2
 git -C ports/ checkout -qf -
 git -C ports/ checkout -qf "$(git -C ports/ merge-base macports/master HEAD)"
 ## Ignore portindex errors on common ancestor
-wait $curl_portindex_pid
+#wait $curl_portindex_pid
 (cd ports/ && portindex)
 git -C ports/ checkout -qf -
 (cd ports/ && portindex -e)
